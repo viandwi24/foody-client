7109d31eb7e66c8dfc69b7c69c8ba12ace92a91d
viandwi24 <viandwicyber@gmail.com>
2022-12-18 00:42:39 +0700

    chore: change api to foody-api.viandwi24.site

--- composables/useHttp.ts
+++ composables/useHttp.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const ax = axios.create({
   // baseURL: 'http://localhost/api/',
-  baseURL: 'https://foody.viandwi24.site/api/',
+  baseURL: 'https://foody-api.viandwi24.site/api/',
 })
 
 export const useHttp = () => {
